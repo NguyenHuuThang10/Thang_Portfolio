8d4481b287e1696752151e96dfc4180510d0e906
NguyenHuuThang10 <nguyenhuuthang@gmail.com>
2025-09-28 11:24:36 +0700

    update logo header

--- src/components/layout/app.header.tsx
+++ src/components/layout/app.header.tsx
@@ -9,6 +9,7 @@ import { useTranslation } from 'react-i18next';
 import { NavDropdown } from 'react-bootstrap';
 import viFlag from 'assets/svg/language/vi.svg';
 import enFlag from 'assets/svg/language/en.svg';
+import logoImg from "assets/logo.png";
 
 type ThemeContextType = "light" | "dark";
 
@@ -40,9 +41,12 @@ function AppHeader() {
         >
             <Container>
                 <Link className="navbar-brand" to="/">
-                    <span className='brand-green'>
-                        {t("appHeader.brand")}
-                    </span>
+                    <img
+                        src={logoImg}
+                        alt="Thang Logo"
+                        height={60} // hoặc 30, tùy bạn muốn cao bao nhiêu
+                        style={{ objectFit: "contain" }}
+                    />
                 </Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
